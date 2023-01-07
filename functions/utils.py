3adf011d252fc73b0ba57560682ee20e7ee89487
Minchang Sung <53217819+tjdalsckd@users.noreply.github.com>
2023-01-08 02:53:14 +0900

    0108
    0108

--- functions/utils.py
+++ functions/utils.py
@@ -1,13 +1,106 @@
 import pybullet as p
 import numpy as np
+np.set_printoptions(precision=4, suppress=True)
+from numpy.linalg import pinv
 import matplotlib.pyplot as plt
+from modern_robotics import *
+import matplotlib.pyplot as plt
+import json
+import pybullet_data
+D2R = np.pi/180.0
+right_joint_list = [b'r_joint_0', b'r_joint_1', b'r_joint_2', b'r_joint_3', b'r_joint_4',b'r_joint_5']
+right_joint_num_list = [2,3,4,5,6,7]
+right_tcp = 8
+left_joint_list = [b'l_joint_0', b'l_joint_1', b'l_joint_2', b'l_joint_3', b'l_joint_4', b'l_joint_5']
+left_joint_num_list = [10, 11, 12, 13, 14, 15]
+left_tcp = 16	
+active_joint_num_list=[2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15]
+MAX_TORQUES = np.array([431.97,431.97,197.23,79.79,79.79,79.79]) # unit [N]
+MAX_TORQUES_ALL = np.array([431.97,431.97,197.23,79.79,79.79,79.79,431.97,431.97,197.23,79.79,79.79,79.79]) # unit [N]
+HomePos = [-0.146 ,-0.814, -1.308, -0.615 ,-1.030 ,0.070, 0.146 ,0.814, 1.308, 0.615 ,1.030 ,-0.070]
 
+def PybulletSetup(p,urdf_name = "model/IndyDualArm.urdf", CONTROL_FREQ = 240.0):
+	## pybullet env setup
+	p.connect(p.GUI)
+	p.setGravity(0, 0, -9.8)
+	p.setRealTimeSimulation(True)
+	timeStep = 1/CONTROL_FREQ
+	p.setTimeStep(timeStep)
+	p.setPhysicsEngineParameter(fixedTimeStep = timeStep)
+	p.setAdditionalSearchPath(pybullet_data.getDataPath())
+	robotId = p.loadURDF(urdf_name, [0, 0, 0.0],[0, 0, 0, 1],flags=p.URDF_USE_SELF_COLLISION)
+	planeId = p.loadURDF("plane.urdf")
+	p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+	p.resetDebugVisualizerCamera(2,90,-15,[0,0,0.5])
+	p.resetBasePositionAndOrientation(robotId, [0, 0, 0], [0, 0, 0, 1])
+	active_joint_list,active_joint_num_list,eef_num=getActiveJointList(robotId)
+	## initialize all joint
+	initializeActiveJoints(robotId,right_joint_num_list)
+	initializeActiveJoints(robotId,left_joint_num_list)
+	dt = 1.0/CONTROL_FREQ
+	return right_joint_num_list,left_joint_num_list,active_joint_num_list,robotId,dt
+def rotMat(theta,axis):
+    R = np.eye(3)
+    if axis == "x":
+        R = np.array([[1 ,0 ,0],[0 ,cos(theta),-sin(theta)],[0 ,sin(theta) ,cos(theta)]])
+    if axis == "y":
+        R = np.array([[cos(theta),0 ,sin(theta)],[0 ,1,0],[-sin(theta),0,cos(theta)]])
+    if axis == "z":
+        R = np.array([[cos(theta),-sin(theta) ,0],[sin(theta) ,cos(theta),0],[0 ,0, 1]])
+    return R
 
-	
-def getJointStates(robotId,active_joints_num):
-	jointState = np.array(p.getJointStates(robotId,active_joints_num))
-	q = np.array(jointState[:,0])
-	qdot = np.array(jointState[:,1])
+def MRSetup(file_path):
+	##MRdata load
+	data={}
+	with open(file_path, 'r') as file:
+	    data = json.load(file)
+	relSlist = np.array(data['relSlist'])
+	relBlist = np.array(data['relBlist'])
+	relM = np.array(data['relM'])
+	baseSlist = np.array(data['baseSlist'])
+	baseM = np.array(data['baseM'])
+	baseBlist = np.array(data['baseBlist'])
+	Tb = np.array(data['Tbr'])
+	return baseSlist,baseBlist,baseM,relSlist,relBlist,relM,Tb
+
+def getJacobian(Slist,Blist,thetalist):
+	thetalist = np.reshape(thetalist,(len(thetalist),1))
+	Js = JacobianSpace(Slist,thetalist);
+	Jb = JacobianSpace(Blist,thetalist);
+	return Js,Jb,pinv(Js),pinv(Jb)
+def reset_q_rel(robotId,q_rel):
+	q_rel_ = [-q_rel[5],-q_rel[4],-q_rel[3],-q_rel[2],-q_rel[1],-q_rel[0],q_rel[6],q_rel[7],q_rel[8],q_rel[9],q_rel[10],q_rel[11]]
+	for i in range(0,len(q_rel_)):
+		p.resetJointState(robotId,active_joint_num_list[i],q_rel_[i])
+def reset_q(robotId,q):
+	for i in range(0,len(q)):
+		p.resetJointState(robotId,active_joint_num_list[i],q[i])
+
+def reset_q_r(q_r):
+	for i in range(0,len(right_joint_num_list)):
+		p.resetJointState(robotId,right_joint_num_list[i],q_r[i])
+
+def reset_q_l(q_l):
+	for i in range(0,len(left_joint_num_list)):
+		p.resetJointState(robotId,left_joint_num_list[i],q_l[i])
+
+def get_q_rel(q_r,q_l):
+	q_rel = [-q_r[5],-q_r[4],-q_r[3],-q_r[2],-q_r[1],-q_r[0],q_l[0],q_l[1],q_l[2],q_l[3],q_l[4],q_l[5]]
+	return np.array(q_rel).T;
+def get_q_r(q_rel):
+	return np.array([-q_rel[5],-q_rel[4],-q_rel[3],-q_rel[2],-q_rel[1],-q_rel[0]]).T
+def get_q_l(q_rel):
+	return np.array([q_rel[6],q_rel[7],q_rel[8],q_rel[9],q_rel[10],q_rel[11]]).T	
+def getJointStates(robotId,j_list):
+	jointState = p.getJointStates(robotId,j_list)
+	q=[]
+	qdot=[]
+	for i in range(0,len(j_list)):
+		js = jointState[i];
+		q.append(js[0])
+		qdot.append(js[1])
+	q = np.array(q)
+	qdot = np.array(qdot)
 	return q,qdot
 def getEEFPose(robotId,eef_num):
 	Teef = np.eye(4);
@@ -62,10 +155,45 @@ def setTorques(robotId, active_joints_num,torques,MAX_TORQUES):
 		idx=idx+1
 	#print("input torques : ", torques)
 	#print("actuated_torques : ", actuated_torques)
-def setPosition(robotId, active_joints_num,position_list,MAX_TORQUES):
+def saveData(data,file_path):
+	for val in data:
+		data[val] =np.array(data[val]).tolist();
+	with open(file_path, 'w') as outfile:
+		json.dump(data, outfile)
+def drawData(file_path):
+	data={}
+	with open(file_path, 'r') as file:
+	    data = json.load(file)
+	t_list = np.array(data["t"])
+	q_rel_list = np.array(data["q_rel"])
+
+	T_rel_list = np.array(data["T_rel"])
+	T_rel_x_list = T_rel_list[:,0,3]
+	T_rel_y_list = T_rel_list[:,1,3]
+	T_rel_z_list = T_rel_list[:,2,3]
+
+	Xd_list = np.array(data["Xd"])
+	Xd_x_list = Xd_list[:,0,3]
+	Xd_y_list = Xd_list[:,1,3]
+	Xd_z_list = Xd_list[:,2,3]
+	#for i in range(0,12):
+	#	plt.plot(t_list,q_rel_list[:,i])
+	plt.subplot(3,1,1)
+	plt.plot(t_list,T_rel_x_list)
+	plt.plot(t_list,Xd_x_list)
+	plt.subplot(3,1,2)
+	plt.plot(t_list,T_rel_y_list)
+	plt.plot(t_list,Xd_y_list)
+	plt.subplot(3,1,3)
+	plt.plot(t_list,T_rel_z_list)
+	plt.plot(t_list,Xd_z_list)
+	plt.tight_layout()
+	plt.show()
+	
+
+def setPosition(robotId, active_joints_num,q_rel,MAX_TORQUES_ALL):
 	idx =0
+	q_rel_ = [-q_rel[5],-q_rel[4],-q_rel[3],-q_rel[2],-q_rel[1],-q_rel[0],q_rel[6],q_rel[7],q_rel[8],q_rel[9],q_rel[10],q_rel[11]]
 	for i in active_joints_num:
-		p.setJointMotorControl2(robotId, i, p.POSITION_CONTROL,targetPosition=position_list[idx], force=MAX_TORQUES[idx])	
+		p.setJointMotorControl2(robotId, i, p.POSITION_CONTROL,targetPosition=q_rel_[idx], force=MAX_TORQUES_ALL[idx])	
 		idx=idx+1
-	#print("input torques : ", torques)
-	#print("actuated_torques : ", actuated_torques)
--- sim.py
+++ sim.py
@@ -1,153 +1,81 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import pybullet as p
 import time
-from math import pi,sqrt,cos,sin
-import pybullet_data
-import numpy as np
-import json
-from modern_robotics import FKinSpace,IKinSpace,CartesianTrajectory,TransInv,Adjoint,JacobianBody,JacobianSpace,se3ToVec,MatrixLog6
-np.set_printoptions(precision=4, suppress=True)
-import math
-import modern_robotics as mr
-from mr_urdf_loader import loadURDF
+from math import *
+from modern_robotics import *
 from functions.utils import *
-import matplotlib.pyplot as plt
 
-def rotMat(theta,axis):
-    R = np.eye(3)
-    if axis == "x":
-        R = np.array([[1 ,0 ,0],[0 ,cos(theta),-sin(theta)],[0 ,sin(theta) ,cos(theta)]])
-    if axis == "y":
-        R = np.array([[cos(theta),0 ,sin(theta)],[0 ,1,0],[-sin(theta),0,cos(theta)]])
-    if axis == "z":
-        R = np.array([[cos(theta),-sin(theta) ,0],[sin(theta) ,cos(theta),0],[0 ,0, 1]])
-    return R
+baseSlist,baseBlist,baseM,relSlist,relBlist,relM,Tb=MRSetup("./MR_info.json");
+r_j_list,l_j_list,a_j_list,robotId,dt=PybulletSetup(p,urdf_name = "model/IndyDualArm.urdf", CONTROL_FREQ = 240.0)
+reset_q(robotId,HomePos)
+endTime = 5;
 
-##MRdata load
-file_path = "./MR_info.json"
+Orn_Kp = 20.0/30.0;
+Orn_Kd = Orn_Kp/20.0;
+Orn_Ki = Orn_Kp/100.0;
+
+Pos_Kp = 20.0
+Pos_Kd = Pos_Kp/20.0;
+Pos_Ki = Pos_Kp/100.0;
+Xe_rel_sum = np.array([0,0,0,0,0,0]).T
 data={}
-with open(file_path, 'r') as file:
-    data = json.load(file)
-relativeSlist = np.array(data['relSlist'])
-relativeBlist = np.array(data['relBlist'])
-relativeM = np.array(data['relM'])
-baseSlist = np.array(data['Slist'])
-baseM = np.array(data['M'])
-baseBlist = np.matmul(Adjoint(TransInv(baseM)),baseSlist)
-Tb = np.array(data['Tbr'])
-Tbl = np.array(data['Tbl'])
+data["t"]=[]
+data["q_rel"]=[]
+data["T_rel"]=[]
+data["Xd"]=[]
+q_l,qdot_l = getJointStates(robotId,l_j_list)
+q_r,qdot_r = getJointStates(robotId,r_j_list)	
+q_rel = get_q_rel(q_r,q_l)
+T_rel = FKinSpace(relM,relSlist,q_rel)
+T_rel_end  = T_rel
+T_rel_end[0,3] = T_rel_end[0,3]+0.2
+T_rel_end[1,3] = T_rel_end[1,3]-0.2
+T_rel_end[2,3] = T_rel_end[2,3]-0.2
+T_rel = FKinSpace(relM,relSlist,q_rel)
+Xd_list = CartesianTrajectory(T_rel, T_rel_end, endTime, int(endTime/dt), 5)
+idx =0
+for t in np.arange(0,endTime,dt):
+	q_l,qdot_l = getJointStates(robotId,l_j_list)
+	q_r,qdot_r = getJointStates(robotId,r_j_list)	
+	q,qdot = getJointStates(robotId,a_j_list)
+	q_rel = get_q_rel(q_r,q_l)
+	T_rel = FKinSpace(relM,relSlist,q_rel)
+	T_r = Tb @  FKinSpace(baseM,baseSlist,q_r)
+	T_l =T_r @ T_rel
+	Js_r,Jb_r,pinvJs_r,pinvJb_r=getJacobian(baseSlist,baseBlist,q_r)
+	Js_rel,Jb_rel,pinvJs_rel,pinvJb_rel = getJacobian(relSlist,relBlist,q_rel)
+	#Xd = np.array([[-1 ,0 ,0 ,0],[0,1 ,0 ,0],[0 ,0 ,-1 ,0.4],[0 ,0 ,0 ,1]])
+	Xd = Xd_list[idx]
+	Xe_rel = se3ToVec(MatrixLog6(TransInv(T_rel)@ Xd))
+	V_rel = Jb_rel@q_rel
+	Xedot_rel = Adjoint(TransInv(T_rel)@ Xd) @ np.array([0,0,0,0,0,0]).T -Xe_rel
+	Xe_rel_sum = Xe_rel_sum+Xe_rel*dt;
 
-## pybullet env setup
-urdf_name = "model/IndyDualArm.urdf"
-p.connect(p.GUI)
-p.setGravity(0, 0, -9.8)
-CONTROL_FREQ = 240
-timeStep = 1/CONTROL_FREQ
-#p.setTimeStep(timeStep)
-p.setPhysicsEngineParameter(fixedTimeStep = timeStep)
-
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-robotId = p.loadURDF(urdf_name, [0, 0, 0.0],[0, 0, 0, 1],flags=p.URDF_USE_SELF_COLLISION)
-p.resetBasePositionAndOrientation(robotId, [0, 0, 0], [0, 0, 0, 1])
-active_joint_list,active_joint_num_list,eef_num=getActiveJointList(robotId)
-
-right_joint_list = [b'r_joint_0', b'r_joint_1', b'r_joint_2', b'r_joint_3', b'r_joint_4',b'r_joint_5']
-right_joint_num_list = [2,3,4,5,6,7]
-right_tcp = 8
-left_joint_list = [b'l_joint_0', b'l_joint_1', b'l_joint_2', b'l_joint_3', b'l_joint_4', b'l_joint_5']
-left_joint_num_list = [10, 11, 12, 13, 14, 15]
-left_tcp = 16
-D2R = np.pi/180.0
-
-HomePos = [0*D2R ,-15*D2R,-90*D2R,0*D2R,-75*D2R,0*D2R,0*D2R ,15*D2R,90*D2R,0*D2R,75*D2R,0*D2R]
-HomePos = [-0.146 ,-0.814, -1.308, -0.615 ,-1.030 ,0.070, 0.146 ,0.814, 1.308, 0.615 ,1.030 ,-0.070]
-## reset joint state
-for i in range(0,len(active_joint_num_list)):
-	p.resetJointState(robotId,active_joint_num_list[i],HomePos[i])
-	
-## initialize all joint
-initializeActiveJoints(robotId,right_joint_num_list)
-initializeActiveJoints(robotId,left_joint_num_list)
-
-MAX_TORQUES = np.array([431.97,431.97,197.23,79.79,79.79,79.79]) # unit [N]
-MAX_TORQUES_ALL = np.array([431.97,431.97,197.23,79.79,79.79,79.79,431.97,431.97,197.23,79.79,79.79,79.79]) # unit [N]
-
-left_qddot = [0,0,0,0,0,0]
-right_qddot = [0,0,0,0,0,0]
+	Kp_Xe_rel = Xe_rel
+	Kp_Xe_rel[0:3] = Orn_Kp*Xe_rel[0:3] 
+	Kp_Xe_rel[3:6] = Pos_Kp*Xe_rel[3:6] 
+	Ki_Xe_rel = Xe_rel_sum
+	Ki_Xe_rel[0:3] = Orn_Ki*Xe_rel_sum[0:3] 
+	Ki_Xe_rel[3:6] = Pos_Ki*Xe_rel_sum[3:6] 
+	Kd_Xe_rel = Xedot_rel
+	Kd_Xe_rel[0:3] = Orn_Kd*Xedot_rel[0:3] 
+	Kd_Xe_rel[3:6] = Pos_Kd*Xedot_rel[3:6] 
 
 
-Xstart = np.eye(4)
-Xstart[0:3,0:3] = np.array([[-1 ,0 ,0],[0 ,0 ,-1],[0 ,-1 ,0]])   
-Xstart[0,3] = 0.5
-Xstart[1,3] = -0.5
-Xstart[2,3] = 0.3
-Xend = np.eye(4)
-Xend[0:3,0:3] =np.array([[-1 ,0 ,0],[0 ,0 ,-1],[0 ,-1 ,0]])  
-Xend[0,3] = 0.5
-Xend[1,3] = 0.5
-Xend[2,3] = 0.3
-
-
-
-endTime = 20;
-dt = timeStep;
-N =  int(endTime/dt);
-
-traj = CartesianTrajectory(Xstart, Xend,endTime , N, 5)
-traj = np.array(traj)
-
-
-for t in np.arange(0,endTime,timeStep):
-	left_q,left_qdot = getJointStates(robotId,left_joint_num_list)
-	right_q,right_qdot = getJointStates(robotId,right_joint_num_list)	
-	jointState = np.array(p.getJointStates(robotId,[2,3,4,5,6,7,10,11,12,13,14,15]))
-	q = np.array(jointState[:,0])
-	qdot = np.array(jointState[:,1])
-	rel_q = [-right_q[5],-right_q[4],-right_q[3],-right_q[2],-right_q[1],-right_q[0],left_q[0],left_q[1],left_q[2],left_q[3],left_q[4],left_q[5]]
-	relT = FKinSpace(relativeM,relativeSlist,np.array(rel_q).T)
-
-	rightT = Tb @  FKinSpace(baseM,baseSlist,right_q)
-	leftT =rightT @ relT
-	
-	print(relT)
-	#rightT_= getEEFPose(robotId,8)
-	#leftT_= getEEFPose(robotId,16)
-	
-
-	
-	Jb_r = JacobianBody(baseBlist,right_q)
-	Js_r = JacobianSpace(baseSlist,right_q)
-	J2 = np.concatenate((-np.flip(Jb_r,1).T,np.zeros([6,6]))).T
-	J1 = JacobianBody(relativeBlist,rel_q)
-	#print(J1)
-	targetT =  TransInv(Tb) @ traj[i,:,:]
-	relative_targetT = np.array([[-1 ,0 ,0 ,0],[0,1 ,0 ,0],[0 ,0 ,-1 ,0.5],[0 ,0 ,0 ,1]])
-
-	print(q)
-	#print(relT)
-	Xe_r = se3ToVec(MatrixLog6(TransInv(rightT)@ targetT))
-	Xe_rel = se3ToVec(MatrixLog6(TransInv(relT)@ relative_targetT))
-	Xe_rel_ = Xe_rel[3:6]
-	pinvJ1 = np.linalg.pinv(J1);
-	pinvJ2 = np.linalg.pinv(J2);
-	J1_ = J1[3:6,:]
-	pinvJ1_ = np.linalg.pinv(J1_);
-	#print(J1.shape)
-	rel_dq = 10*pinvJ1@ Xe_rel       
-	rel_q = rel_q+rel_dq*timeStep;
-	right_q_ = -np.flip(rel_q[0:6])
-	left_q_ = rel_q[6:12]
-	
-
-	ID = p.calculateInverseDynamics(robotId,[q[0],q[1],q[2],q[3],q[4],q[5],q[6],q[7],q[8],q[9],q[10],q[11]],[qdot[0],qdot[1],qdot[2],qdot[3],qdot[4],qdot[5],qdot[6],qdot[7],qdot[8],qdot[9],qdot[10],qdot[11]],[0,0,0,0,0,0,0,0,0,0,0,0]);
-	#setTorques(robotId, active_joint_num_list,ID,MAX_TORQUES_ALL)
-	#setTorques(robotId, right_joint_num_list,right_ID,MAX_TORQUES)
-	setPosition(robotId, right_joint_num_list,right_q_,MAX_TORQUES)	
-	setPosition(robotId, left_joint_num_list,left_q_,MAX_TORQUES)		
+	Xe_rell_All = Kp_Xe_rel+Kd_Xe_rel+Ki_Xe_rel
+	#qdot_rel = 20*pinvJb_rel[:,3:6] @ Xe_rel[3:6]
+	qdot_rel = 20*pinvJb_rel @ Xe_rell_All
+	q_rel,qdot_rel=EulerStep(q_rel,qdot_rel,np.zeros((12,1)),dt)	
+	q_r=get_q_r(q_rel);
+	q_l=get_q_l(q_rel);
+	setPosition(robotId, a_j_list,q_rel,MAX_TORQUES_ALL)
+	print(t)		
+	data["t"].append(t)
+	data["q_rel"].append(q_rel)
+	data["T_rel"].append(T_rel)
+	data["Xd"].append(Xd)
+	idx = idx+1
 	p.stepSimulation()
-	time.sleep(timeStep);
-
-
+saveData(data,"output.json")	
+drawData("output.json")	
 
 
